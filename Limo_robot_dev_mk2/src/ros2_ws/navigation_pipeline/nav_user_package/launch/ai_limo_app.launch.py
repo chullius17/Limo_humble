abe65c12039ac47659cac987d27169bc73805501
Chullius17 <cataldogiulio17@gmail.com>
2026-08-18 20:21:42 +0200

    Double names adjusted in navigation

---
+++ Limo_robot_dev_mk2/src/ros2_ws/navigation_pipeline/nav_user_package/launch/ai_limo_app.launch.py
@@ -0,0 +1,62 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    use_controller = LaunchConfiguration('use_controller')
+    ai_mode = LaunchConfiguration('ai_mode')
+
+    use_controller_arg = DeclareLaunchArgument(
+        'use_controller',
+        default_value='true',
+        description='Start and connect the trajectory controller'
+    )
+
+    ai_mode_arg = DeclareLaunchArgument(
+        'ai_mode',
+        default_value='true',
+        description='Use AI-specific control-log storage'
+    )
+
+    trajectory_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('ai_traj_package'),
+                'launch',
+                'ai_trajectory.launch.py'
+            )
+        ),
+        launch_arguments={'use_controller': use_controller}.items()
+    )
+
+    control_node = Node(
+        package='nav_limo_controller', 
+        executable='controller',
+        name='controller',
+        output='screen',
+        condition=IfCondition(use_controller)
+    )
+
+    user_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('nav_user_package'),
+                'launch',
+                'user.launch.py'
+            )
+        ),
+        launch_arguments={'ai_mode': ai_mode}.items()
+    )
+
+    return LaunchDescription([
+        use_controller_arg,
+        ai_mode_arg,
+        trajectory_launch,
+        control_node,
+        user_launch
+    ])
